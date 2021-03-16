f6e9c3b685852b89299f0f68d16d17726c7fd5a7
Rem0o <rem.merc@outlook.com>
2021-03-16 14:33:10 -0400

    Initial commit

---
+++ HWInfoPlugin.cs
@@ -0,0 +1,37 @@
+﻿using FanControl.Plugins;
+
+namespace FanControl.HWInfo
+{
+    public class HWInfoPlugin : IPlugin
+    {
+        private HWInfo _hWInfo;
+
+        public string Name => "HWInfo";
+
+        public void Close()
+        {
+            _hWInfo?.Dispose();
+            _hWInfo = null;
+        }
+
+        public void Initialize() => _hWInfo = new HWInfo();
+
+        public void Load(IPluginSensorsContainer _container)
+        {
+            foreach (var source in _hWInfo?.SensorsSource)
+                foreach(var sensor in source.Sensors)
+                    AddSensorToContainer(_container, source, sensor);
+        }
+
+        private static void AddSensorToContainer(IPluginSensorsContainer _container, HWInfo.HWInfoSensorSource source, HWInfo.HWInfoSensor sensor)
+        {
+            var sensorElement = sensor.GetUpdatedElement();
+            var pluginSensor = new HWInfoPluginSensor(source.Sensor.szSensorNameUser, sensorElement.szLabelUser, sensor.GetUpdatedElement);
+
+            if (sensorElement.szUnit == "RPM")
+                _container.FanSensors.Add(pluginSensor);
+            else if (sensorElement.szUnit == "°C")
+                _container.TempSensors.Add(pluginSensor);
+        }
+    }
+}
---
+++ HWInfoPluginSensor.cs
@@ -0,0 +1,22 @@
+﻿using FanControl.Plugins;
+using System;
+
+namespace FanControl.HWInfo
+{
+    public class HWInfoPluginSensor : IPluginSensor
+    {
+        private readonly Func<HWInfo._HWiNFO_ELEMENT> _sensorElementGetter;
+
+        public HWInfoPluginSensor(string originName, string name, Func<HWInfo._HWiNFO_ELEMENT> sensorElementGetter)
+        {
+            Name = $"{name} - {originName}";
+            _sensorElementGetter = sensorElementGetter;
+        }
+
+        public string Name { get; }
+
+        public float? Value { get; private set; }
+
+        public void Update() => Value = (float)_sensorElementGetter().Value;
+    }
+}
